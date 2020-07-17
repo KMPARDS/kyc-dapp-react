2e2bc1a6d799ad8e7a0ece6fb2a8ea53ae753345
mahesh <maheshrapelli141@gmail.com>
2020-07-17 16:47:43 +0530

    level three content modified

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,7 @@ module.exports = {
     "image/png"
   ],
   FILE_SIZE: 10000000,
-  ACCEPT_PRESET: 'I hereby declare and authorize info@eraswapfoundation.com to migrate all my TimeAlly Staking of 1 Year / 2 Year to TimeAlly 1 Life 1 Time Staking to participate in Era Swap Network Proof of Stake Consensus Protocol (ESN PoS CP). I have read, understood and agree to TimeAlly Terms and Conditions. \n\nFull Name:\nContact Number:',
-  REJECT_PRESET: "I hereby decline to migrate all my TimeAlly Staking of 1 Year / 2 Year to TimeAlly 1 Life 1 Time Staking to participate in Era Swap Network Proof of Stake Consensus Protocol (ESN PoS CP). I am responsible for it. \n\nFull Name:\nContact Number:",
+  ACCEPT_PRESET: 'I hereby declare and authorize info@eraswapfoundation.com to migrate all my TimeAlly Staking to TimeAlly 1 Life Time Staking to participate in Era Swap Network Proof of Stake Consensus Protocol (ESN PoS CP). I have read, understood and agree to Era Swap Terms and Conditions & Era Swap Whitepaper (https://eraswaptoken.io/pdf/eraswap_whitepaper.pdf)\n\nFull Name:\nContact Number:',  REJECT_PRESET: "I hereby decline to migrate all my TimeAlly Staking of 1 Year / 2 Year to TimeAlly 1 Life 1 Time Staking to participate in Era Swap Network Proof of Stake Consensus Protocol (ESN PoS CP). I am responsible for it. \n\nFull Name:\nContact Number:",
   ACCEPT_ESN: 'accept',
   REJECT_ESN: 'reject',
 }
