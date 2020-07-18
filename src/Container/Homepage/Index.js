5976c588a612ff96a08218fd6a754d2d1ce058c5
sanket403 <sanketparab403@gmail.com>
2020-07-18 16:02:16 +0530

    start kyc popuup added unwanted content remove 18july

--- src/Container/Homepage/Index.js
+++ src/Container/Homepage/Index.js
@@ -71,20 +71,8 @@ class Homepage extends Component {
                     <Section/>
                 </div>
                 <div>
-                <div className="next-gen">
-                    <Container>
-                        <div className="col-12 col-lg-10 mx-auto text-center reveal">
-                            <h2 className="mb-3-head"><span class="color--primary">Next generation</span> Decentralized Platforms <br /> of Era Swap .</h2>
-                            <p className="mb-4-para mt30">The potential of Blockchain technology to disrupt nearly every industry and solve its current flaws cannot be dismissed. With Era Swap Utility you can access to a secure, reliable and self-sustainable the decentralized network consists of multiple utility platforms and middlemen free marketplaces for the community. The most beneficial fact about Era Swap decentralized platforms is that it is not controlled by any central authority means you doesn’t have to worry about multi-layer charges nor your data can be accessed by any Central Authority for their own benefits  </p>
-                        </div>
-
-                    </Container>
-                </div>
-                <div className="svg-shape--top w-100 z-index1">
-                    <svg viewBox="0 0 1920 522.59" xmlns="" className="svg fill-primary-light--1 w-100 replaced-svg">
-                    <path d="M1920,0V453.47c-52.38-56.59-216.06,206.54-400-34.22q-20.55-26.89-41.39-52.16C1303.07,154.18,1114.33,29.34,960,25.08c-154.33,4.26-343.07,129.1-518.61,342Q420.56,392.35,400,419.25C216.06,660,52.38,396.88,0,453.47V0Z"></path></svg>
-                </div>
-                    <img className="v-shape-img-style" src={Images.path.vshap}/>
+            
+                  
                     </div>
                 </div>
 
