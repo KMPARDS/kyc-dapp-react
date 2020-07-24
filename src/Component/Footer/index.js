bdd60da273bf4961893fe3d46ff237e38b2b703a
sanket403 <sanketparab403@gmail.com>
2020-07-24 11:16:26 +0530

    footer changes sequence 24july

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -4,7 +4,7 @@ import Images from '../../Container/Images/Images';
 import { Col, Button, Container, Row } from 'react-bootstrap';
 import TC from '../../assets/docs/tc.pdf'
 import Userguide from '../../assets/docs/KYC_DAPP_UserGuide.pdf'
-
+import Prp from '../../assets/docs/kyc-dapp-privacy-policy.pdf'
 class Footer extends Component {
 
     constructor(props) {
@@ -28,26 +28,24 @@ class Footer extends Component {
                                     <p className="logo-sub-txt mt30"></p>
                                 </Col>
                                 <Col xl={4} lg={4} md={4} col={12}>
-                                    <h4 className="footer-head">Useful Link</h4>
+                                    <h4 className="footer-head">Useful Links</h4>
                                     <ul>
-                                        <li><a className="footer-sub" href="#home">Home</a></li>
-                                        <li><a className="footer-sub" href="#solution">Solutions</a></li>
-                                        <li><a className="footer-sub" href="#verification">Verification</a></li>
-                                        <li><a className="footer-sub" href="#features">Features</a></li>
-                                        <li><a href="http://eraswaptoken.io/pdf/es-aml-policy.pdf" target='_blank' className="footer-sub">AML Policy</a></li>
                                         <li><a href="http://eraswaptoken.io/pdf/es-disclaimer.pdf" target='_blank' className="footer-sub">Disclaimer</a></li>
                                         <li><a href="http://eraswaptoken.io/pdf/es-statuary-warning.pdf" target='_blank' className="footer-sub">Statuary Warning</a></li>
+                                        <li><a href={Prp} target='_blank' className="footer-sub">Privacy Policy</a></li>
+                                        <li><a href="http://eraswaptoken.io/pdf/es-aml-policy.pdf" target='_blank' className="footer-sub">AML Policy</a></li>   
+                                        <li><a className="footer-sub" href="https://eraswaptoken.io/pdf/eraswap-terms-conditions.pdf" target="_blank">Era Swap Terms & Conditions</a></li>
                                     </ul>
                                 </Col>
                                 <Col offset-xl={1} xl={3} lg={4} md={4} col={12}>
-                                    <h4 className="footer-head">Documents</h4>
+                                    <h4 className="footer-head">&nbsp;</h4>
                                     <ul>
-                                               <li><a href={Userguide} target='_blank' className="footer-sub">User Guide</a></li>
-                                        <li><a className="footer-sub">Privacy Policy</a></li>
+                                        <li><a className="footer-sub" href="#home">Home</a></li>
+                                         <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/eraswap_whitepaper.pdf" target="_blank">ES White Paper </a></li>
+                                         <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/era-swap-howey-test-letter-august7-2018.pdf" target="_blank">Howey Test</a></li>
+                                         <li><a href={Userguide} target='_blank' className="footer-sub">User Guide</a></li>
                                         <li><a href={TC} target='_blank' className="footer-sub">Terms of Use</a></li>
-                                        <li><a className="footer-sub" href="https://eraswaptoken.io/pdf/eraswap-terms-conditions.pdf" target="_blank">Era Swap Terms & Conditions</a></li>
-                                        <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/era-swap-howey-test-letter-august7-2018.pdf" target="_blank">Howey Test</a></li>
-                                        <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/eraswap_whitepaper.pdf" target="_blank">ES White Paper </a></li>
+                                    
                                     </ul>
                                 </Col>
                             </Row>
