da1b551e6f8f0fd160e4279a5d6657e3b0d03204
sanket403 <sanketparab403@gmail.com>
2020-07-29 14:08:08 +0530

    added social media and mohit content edited 29 july"

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -1,98 +1,215 @@
 import React, { Component } from 'react';
-import './Footer.css'
+import './Footer.css';
 import Images from '../../Container/Images/Images';
 import { Col, Button, Container, Row } from 'react-bootstrap';
-import TC from '../../assets/docs/tc.pdf'
-import Userguide from '../../assets/docs/KYC_DAPP_UserGuide.pdf'
-import Prp from '../../assets/docs/kyc-dapp-privacy-policy.pdf'
+import TC from '../../assets/docs/tc.pdf';
+import Userguide from '../../assets/docs/KYC_DAPP_UserGuide.pdf';
+import Prp from '../../assets/docs/kyc-dapp-privacy-policy.pdf';
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
+  render() {
+    return (
+      <div>
+        <div className="Footer-wrapper-container">
+          <div className="Footer-container">
+            <Container>
+              <Row>
+                <Col lg={5} md={9} col={12}>
+                  <img
+                    className="logo-img-style"
+                    src={Images.path.kycdapwhite}
+                  />
+                  <p className="logo-sub-txt mt30"></p>
+                </Col>
+                <Col xl={4} lg={4} md={4} col={12}>
+                  <h4 className="footer-head">Useful Links</h4>
+                  <ul>
+                    <li>
+                      <a
+                        href="http://eraswaptoken.io/pdf/es-disclaimer.pdf"
+                        target="_blank"
+                        className="footer-sub"
+                      >
+                        Disclaimer
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="http://eraswaptoken.io/pdf/es-statuary-warning.pdf"
+                        target="_blank"
+                        className="footer-sub"
+                      >
+                        Statuary Warning
+                      </a>
+                    </li>
+                    <li>
+                      <a href={Prp} target="_blank" className="footer-sub">
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="http://eraswaptoken.io/pdf/es-aml-policy.pdf"
+                        target="_blank"
+                        className="footer-sub"
+                      >
+                        AML Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="footer-sub"
+                        href="https://eraswaptoken.io/pdf/eraswap-terms-conditions.pdf"
+                        target="_blank"
+                      >
+                        Era Swap Terms & Conditions
+                      </a>
+                    </li>
+                  </ul>
+                </Col>
+                <Col offset-xl={1} xl={3} lg={4} md={4} col={12}>
+                  <h4 className="footer-head">&nbsp;</h4>
+                  <ul>
+                    <li>
+                      <a className="footer-sub" href="#home">
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="footer-sub"
+                        href="http://eraswaptoken.io/pdf/eraswap_whitepaper.pdf"
+                        target="_blank"
+                      >
+                        ES White Paper{' '}
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="footer-sub"
+                        href="http://eraswaptoken.io/pdf/era-swap-howey-test-letter-august7-2018.pdf"
+                        target="_blank"
+                      >
+                        Howey Test
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href={Userguide}
+                        target="_blank"
+                        className="footer-sub"
+                      >
+                        User Guide
+                      </a>
+                    </li>
+                    <li>
+                      <a href={TC} target="_blank" className="footer-sub">
+                        Terms of Use
+                      </a>
+                    </li>
+                  </ul>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+          <div className="bottom-footer">
+            <div className="">
+              <Container>
+                <Row>
+                  <Col sm={4}>
+                    <p className="logo-sub-para">
+                      © <a className="footer-sub">Eraswap</a> 2020 | All rights
+                      reserved
+                    </p>
+                  </Col>
 
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="Footer-wrapper-container">
-
-                    <div className="Footer-container">
-
-                        <Container>
-                            <Row>
-                                <Col lg={5} md={9} col={12}>
-                                    <img className="logo-img-style" src={Images.path.kycdapwhite} />
-                                    <p className="logo-sub-txt mt30"></p>
-                                </Col>
-                                <Col xl={4} lg={4} md={4} col={12}>
-                                    <h4 className="footer-head">Useful Links</h4>
-                                    <ul>
-                                        <li><a href="http://eraswaptoken.io/pdf/es-disclaimer.pdf" target='_blank' className="footer-sub">Disclaimer</a></li>
-                                        <li><a href="http://eraswaptoken.io/pdf/es-statuary-warning.pdf" target='_blank' className="footer-sub">Statuary Warning</a></li>
-                                        <li><a href={Prp} target='_blank' className="footer-sub">Privacy Policy</a></li>
-                                        <li><a href="http://eraswaptoken.io/pdf/es-aml-policy.pdf" target='_blank' className="footer-sub">AML Policy</a></li>   
-                                        <li><a className="footer-sub" href="https://eraswaptoken.io/pdf/eraswap-terms-conditions.pdf" target="_blank">Era Swap Terms & Conditions</a></li>
-                                    </ul>
-                                </Col>
-                                <Col offset-xl={1} xl={3} lg={4} md={4} col={12}>
-                                    <h4 className="footer-head">&nbsp;</h4>
-                                    <ul>
-                                        <li><a className="footer-sub" href="#home">Home</a></li>
-                                         <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/eraswap_whitepaper.pdf" target="_blank">ES White Paper </a></li>
-                                         <li><a className="footer-sub" href="http://eraswaptoken.io/pdf/era-swap-howey-test-letter-august7-2018.pdf" target="_blank">Howey Test</a></li>
-                                         <li><a href={Userguide} target='_blank' className="footer-sub">User Guide</a></li>
-                                        <li><a href={TC} target='_blank' className="footer-sub">Terms of Use</a></li>
-                                    
-                                    </ul>
-                                </Col>
-                            </Row>
-                        </Container>
-
+                  <Col sm={8}>
+                    <div className="footer-social-flex">
+                      <ul className="social-menu">
+                        <li>
+                          <a
+                            href="https://www.facebook.com/eraswap"
+                            target="_blank"
+                          >
+                            <i className="fa fa-facebook"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.linkedin.com/company/eraswap/"
+                            target="_blank"
+                          >
+                            <i className="fa fa-linkedin"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/eraswap/?hl=en"
+                            target="_blank"
+                          >
+                            <i className="fa fa-instagram"></i>
+                          </a>{' '}
+                        </li>
+                        <li>
+                          <a
+                            href="https://twitter.com/EraSwapTech"
+                            target="_blank"
+                          >
+                            <i className="fa fa-twitter"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://medium.com/@eraswap" target="_blank">
+                            <i className="fa fa-medium"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://eraswap.tumblr.com/" target="_blank">
+                            <i className="fa fa-tumblr"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://t.me/eraswap" target="_blank">
+                            <i className="fa fa-telegram"></i>
+                          </a>
+                        </li>
+                        <li>
+                          {' '}
+                          <a href="https://github.com/KMPARDS" target="_blank">
+                            <i className="fa fa-github"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.reddit.com/user/EraSwap"
+                            target="_blank"
+                          >
+                            <i className="fa fa-reddit"></i>
+                          </a>{' '}
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.youtube.com/channel/UCGCP4f5DF1W6sbCjS6y3T1g?view_as=subscriber"
+                            target="_blank"
+                          >
+                            <i className="fa fa-youtube"></i>
+                          </a>
+                        </li>
+                      </ul>
                     </div>
-                    <div className="bottom-footer">
-                        <div className="">
-                            <Container>
-                            <Row>
-
-                                <Col sm={6}>
-                                    <p class="logo-sub-para">© <a class="footer-sub">Eraswap</a> 2020  | All rights reserved</p>
-                                </Col>
-
-                                <Col sm={6}>
-                                    <div className="footer-social-flex">
-                                    <a href="https://www.facebook.com/eraswap" className="social-icon">
-                                        <img className="social-img-footer" src={Images.path.grpfb} />
-                                        </a>
-                                        <a href="https://mobile.twitter.com/EraSwapTech" className="social-icon">
-                                        <img className="social-img-footer" src={Images.path.whitetwt} />
-                                        </a>
-                                        <a href="https://www.linkedin.com/company/eraswap/"  className="social-icon">
-                                        <img className="social-img-footer" src={Images.path.whIn} />
-                                        </a>
-                                        <a href="https://www.instagram.com/eraswap/?hl=en" className="social-icon">
-                                        <img className="social-img-footer" src={Images.path.whtinsta} />
-                                        </a>
-                                        <a href="https://www.youtube.com/channel/UCGCP4f5DF1W6sbCjS6y3T1g?view_as=subscriber" className="social-icon">
-                                        <img className="social-img-footer" src={Images.path.utubwht} />
-                                        </a>
-                                    </div>
-                                </Col>
-
-                            </Row>
-                            </Container>
-                        </div>
-                    </div>
-
-                </div>
+                  </Col>
+                </Row>
+              </Container>
             </div>
-
-        );
-
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Footer;
--- src/Container/Homepage/Index.js
+++ src/Container/Homepage/Index.js
@@ -6,81 +6,96 @@ import Hero from '../../Component/Hero/Index';
 import Section from '../../Component/Section/Index';
 import Images from '../../Container/Images/Images';
 
-
 class Homepage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
-    
+  render() {
+    return (
+      <div>
+        <div className="wrapper-container-home">
+          <Hero />
+        </div>
+        <div className="future-container" id="solution">
+          <h2 className="future-head">
+            The future of Identity
+            <br /> <span className="red-color-txt">Verification</span> is here
+          </h2>
+          <p className="para-txt-future">
+            To stay complaint with regulators and to stop sophisticated
+            financial crimes, KYC DApp is being created{' '}
+          </p>
+          <div className="kyc-feature-circle">
+            <Container>
+              <Row>
+                <Col lg={4} md={6} sm={8} className="mx-auto">
+                  <div className="feature-centr">
+                    <img className="feature-Img" src={Images.path.grp1} />
+                  </div>
+                  <div>
+                    <h4 className="feature-head">Transparency</h4>
+                    <hr
+                      className="bg-color--primary border--none mx-auto  jsElement dash-red"
+                      data-height="3"
+                      data-width="80"
+                    />
+                    <p className="text-center">
+                      {' '}
+                      KYC DApp, complaint documents are stored on blockchain
+                      which makes it transparent and secure
+                    </p>
+                  </div>
+                </Col>
 
-    render() {
-        return (
-            <div >
-                <div className='wrapper-container-home'>
-                    <Hero/>
-                </div>
-                <div className="future-container" id="solution">
-                    <h2 className="future-head">The future of Identity<br /> <span className="red-color-txt">Verification</span> is here</h2>
-                    <p className="para-txt-future">
-                        1.  To stay complaint with regulators and to stop sophisticated financial crimes, KYC DApp is being created </p>
-                    <div className="kyc-feature-circle">
-                        <Container>
-                            <Row>
+                <Col lg={4} md={6} sm={8} className="mx-auto">
+                  <div className="feature-centr">
+                    <img className="feature-Img" src={Images.path.grp2} />
+                  </div>
+                  <div>
+                    <h4 className="feature-head">Era Swap Ecosystem</h4>
+                    <hr
+                      className="bg-color--primary border--none mx-auto  jsElement dash-red"
+                      data-height="3"
+                      data-width="80"
+                    />
+                    <p className="text-center">
+                      Via KYC DApp, a customer could update their personal
+                      information across all the platforms of Era Swap Ecosystem
+                    </p>
+                  </div>
+                </Col>
 
-                                <Col lg={4} md={6} sm={8} className="mx-auto">
-                                    <div className="feature-centr">
-                                         <img className='feature-Img' src={Images.path.grp1} />
-                                    </div>
-                                    <div>
-                                        <h4 className="feature-head">Transparency</h4>
-                                        <hr className="bg-color--primary border--none mx-auto  jsElement dash-red" data-height="3" data-width="80"/>
-                                        <p className="text-center">   KYC DApp, complaint documents are stored on blockchain which makes it transparent and secure</p>
-                                    </div>
-                                </Col>
+                <Col lg={4} md={6} sm={8} className="mx-auto">
+                  <div className="feature-centr">
+                    <img className="feature-Img" src={Images.path.grp3} />
+                  </div>
+                  <div>
+                    <h4 className="feature-head">One Stop</h4>
+                    <hr
+                      className="bg-color--primary border--none mx-auto  jsElement dash-red"
+                      data-height="3"
+                      data-width="80"
+                    />
+                    <p className="text-center">
+                      This One Stop KYC for multiple platforms of ESE will save
+                      repetitive and reiterative work.{' '}
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
 
-                                <Col lg={4} md={6} sm={8} className="mx-auto">
-                                    <div className="feature-centr">
-                                        <img className='feature-Img' src={Images.path.grp2} />
-                                    </div>
-                                    <div>
-                                        <h4 className="feature-head">Era Swap Ecosystem</h4>
-                                        <hr className="bg-color--primary border--none mx-auto  jsElement dash-red" data-height="3" data-width="80"/>
-                                        <p className="text-center">Via KYC DApp, a customer could update their personal information across all the platforms of Era Swap Ecosystem</p>
-                                    </div>
-                                </Col>
-
-                                <Col lg={4} md={6} sm={8} className="mx-auto">
-                                    <div className="feature-centr">
-                                        <img className='feature-Img' src={Images.path.grp3} />
-                                    </div>
-                                    <div>
-                                        <h4 className="feature-head">One Stop</h4>
-                                        <hr className="bg-color--primary border--none mx-auto  jsElement dash-red" data-height="3" data-width="80"/>
-                                        <p className="text-center">This One Stop KYC for multiple platforms of ESE will save repetitive and reiterative work. </p>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </div>
-
-                <div className="kyc-section-wrapper">
-                    <Section/>
-                </div>
-                <div>
-            
-                  
-                    </div>
-                </div>
-
-            </div>
-
-        );
-
-    }
+          <div className="kyc-section-wrapper">
+            <Section />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Homepage;
