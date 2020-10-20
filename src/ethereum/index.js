f337b93c09ded2eafc8c0970ecdbf4a71f896e35
maheshrapelli141 <maheshrapelli141@gmail.com>
2020-10-20 18:36:46 +0530

    ethereum network set to development

--- src/ethereum/index.js
+++ src/ethereum/index.js
@@ -1,11 +1,14 @@
 import {CustomProvider, typechain,addresses } from 'eraswap-sdk';
 
-export const providerESN = new CustomProvider(process.env.REACT_APP_NODE_ENV === 'development' ? 'testnet' : 'mainnet');
+//export const providerESN = new CustomProvider(process.env.REACT_APP_NODE_ENV === 'development' ? 'testnet' : 'mainnet');
+export const providerESN = new CustomProvider('testnet');
 
-export const kycInst = typechain.ESN.KycDappFactory.connect(addresses[process.env.REACT_APP_NODE_ENV].ESN.kycdapp,providerESN);
+//export const kycInst = typechain.ESN.KycDappFactory.connect(addresses[process.env.REACT_APP_NODE_ENV].ESN.kycdapp,providerESN);
+export const kycInst = typechain.ESN.KycDappFactory.connect(addresses['development'].ESN.kycdapp,providerESN);
 
 export const dayswappersInst = typechain.ESN.DayswappersWithMigrationFactory.connect(
-  addresses[process.env.NODE_ENV].ESN.dayswappers,
+ // addresses[process.env.NODE_ENV].ESN.dayswappers,
+addresses['development'].ESN.dayswappers,
   providerESN
 );
 
